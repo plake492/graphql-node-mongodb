4fb7c6d82886832765868aea475101fbefbd4174
plake <plake492@gmail.com>
2019-10-30 23:33:05 -0400

    finished basic tutorial

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://localhost:27017/graphqldb';
-
+const url =
+  "mongodb+srv://plake492:doLl3ZlD0ZU3uxdK@test-i2ely.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true });
-mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
+mongoose.connection.once("open", () =>
+  console.log(`Connected to mongo at ${url}`)
+);
